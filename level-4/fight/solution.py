0eb3fd107a44921ff90b5f95076b39d194bf6e1f
Matt Falcioni <mfalcioni1@gmail.com>
2022-02-05 15:26:05 -0500

    researching

---
+++ level-4/fight/solution.py
@@ -0,0 +1,15 @@
+def solution(dimensions, your_position, trainer_position, distance):
+    """
+    Figure out the max number of bounces a shot can take given distance
+    constraint?
+    There has to be some property vectors originating from each point and
+    the angle they produce when intersecting on of the walls
+
+    https://www.sciencedirect.com/topics/mathematics/angle-between-two-vector
+    https://en.wikipedia.org/wiki/Angle_of_incidence_(optics)
+    movement is alot like billiards table
+    https://en.wikipedia.org/wiki/Dynamical_billiards
+    
+    """
+
+    return 0 
