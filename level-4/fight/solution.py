63a76fbc78e8686f2334210b1b7fc556af955e31
Matt Falcioni <mfalcioni1@gmail.com>
2022-02-14 21:07:48 -0500

    we got there, now to code it up

--- level-4/fight/solution.py
+++ level-4/fight/solution.py
@@ -16,20 +16,16 @@ def solution(dimensions, your_position, trainer_position, distance):
 
     https://www.khanacademy.org/math/geometry/hs-geo-similarity/hs-geo-similar-and-congruent-triangles-modeling/v/triangle-similarity-in-pool
     https://en.wikipedia.org/wiki/Carom_billiards
+    
     https://undergroundmathematics.org/thinking-about-geometry/r8022/solution
-    The distance of the ball from the rail being targeted
-    to the target ball creates similiar triangles
-    if they are equidistant from the rails then the triangles are congruent
+    Method 1 from this link creates a helpful visual. 
+    https://plus.maths.org/content/arithmetic-billiards-0
+    Combined with this I think there is a concept that can be applied, "unfolding the billiards table"
+    with each "fold" (reflection of the table off a given wall) we replace our opponent in space
+    then after "x" folds we calculate the distance between us and the folded opponent
+    if it is < distance constraint then it is a viable solution.
 
-    I believe the net result is that each carom produces a new either
-    similiar or congruent triangle of which we need to calculate the one side
-    of to see if it violates the distance constraint. Then we need to find
-    the carom counting pattern. i.e. 1 carom not on the same x or y produces
-    4 possible solutions (2 pairs of symmetric solutions), 
-    whereas equal y or x limits it to two.
-
-    Other rules, odd numbers of caroms produce "Vs" and even numbers
-    of caroms produce polygons.
+    This works. I do have to figure out a way to check that you don't hit yourself.
     """
     def dist(p_1, p_2):
         return ((p_2[0] - p_1[0])**2 + (p_2[1] - p_1[1])**2)**0.5
@@ -38,12 +34,31 @@ def solution(dimensions, your_position, trainer_position, distance):
         i = 1
     else:
         i = 0
-
-    if your_position[0] == trainer_position[0] or your_position[1] == trainer_position[1]:
-        # we construct a system of equations that follow the form
-        # x/your_position[0] = (your_position[1] - x)/(train_position[0] - y)
-        # (x/your_position[0]) = (trainer_position[])
     
+    e_wall = [dimensions[0], trainer_position[1]]
+    w_wall = [0, trainer_position[1]]
+    n_wall = [trainer_position[0], dimensions[1]]
+    s_wall = [trainer_position[0], 0]
+
+    def s_fold(yp, tp, e_wall, w_wall, n_wall, s_wall):
+        return [tp[0], tp[1] - 2*dist(tp, s_wall)]
+    
+    def n_fold(tp):
+        return [tp[0], tp[1] + 2*dist(tp, n_wall)]
+    
+    def e_fold(tp):
+        return [tp[0] + 2*dist(tp, e_wall), tp[1]]
+    
+    def w_fold(tp):
+        return [tp[0] - 2*dist(tp, w_wall), tp[1]]
+    
+
+    dist(your_position, n_fold(trainer_position))
 
     return i
 
+# test case
+dimensions = [3, 2]
+your_position = [1, 1]
+trainer_position = [2, 1]
+distance = 4
